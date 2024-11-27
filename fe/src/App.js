437009de7e01eba5e54d7f31dcaa5b8765c91894
Bahram Heidari <great.bahram@gmail.com>
2024-11-27 20:47:22 +0100

    first commit

---
+++ fe/src/App.js
@@ -0,0 +1,13 @@
+import Timeline from './components/News';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Test app</p>
+      <Timeline />
+    </div>
+  );
+}
+
+export default App;
